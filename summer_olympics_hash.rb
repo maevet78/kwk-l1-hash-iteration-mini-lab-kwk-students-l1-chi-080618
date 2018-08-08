2470dc589458ed230748e8067c9c4de869af4a3e
maevet78 <190439mt@saintviator.com>
2018-08-08 19:57:43 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -18,10 +18,10 @@ def add_a_key_value_pair
 end
 
 
-# def iterate_through_hash
-#   # Implement this method so that it iterates over the hash created in add_a_key_value_pair
-#   # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
-# end
+def iterate_through_hash
+  # Implement this method so that it iterates over the hash created in add_a_key_value_pair
+  # and puts each key value pair within the phrase "The _____ summer olympics took place in _____."
+end
 
 # def iterate_through_keys
 #   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
