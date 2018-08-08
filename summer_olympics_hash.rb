c2b6110fcd0574108f990e8a46c4bd44ef02d372
maevet78 <190439mt@saintviator.com>
2018-08-08 19:45:47 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -1,10 +1,10 @@
 
 def create_olympics_hash
  summer_olympics = {
-  :Sydney => 2000
-  :Athens => 2004
-  :Beijing => 2008
-  :London => 2012
+  :Sydney => "2000"
+  :Athens => "2004"
+  :Beijing => "2008"
+  :London => "2012"
 }
 end
 create_olympics_hash
